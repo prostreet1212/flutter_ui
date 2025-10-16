cd9429c15e7020b96b5f046ec8f645f9d75370d2
user <prostreet1212@gmail.com>
2025-10-16 16:54:54 +0300

    add category food

---
+++ food_delivery_app_ui/lib/pages/home_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app_ui/widgets/app_bar_widget.dart';
+
+import '../widgets/categories_widget.dart';
+import '../widgets/popular_item_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          //Appbar
+          AppBarWidget(),
+          //Search
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withValues(alpha: 0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
+                child: Row(
+                  children: [
+                    Icon(CupertinoIcons.search, color: Colors.red),
+                    Container(
+                      height: 50,
+                      width: 300,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Что вы ищете?',
+                            //hintStyle: TextStyle(colo r: Colors.green),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.filter_list),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          //Category
+          Padding(
+              padding: EdgeInsets.only(top: 10,left: 10),
+          child: Text('Категории',style: TextStyle(fontWeight: FontWeight.bold,
+          fontSize: 20),),
+          ),
+          //Category widget
+          CategoriesWidget(),
+          Padding(
+            padding: EdgeInsets.only(top: 10,left: 10),
+            child: Text('Популярное',style: TextStyle(fontWeight: FontWeight.bold,
+                fontSize: 20),),
+          ),
+          //Popular Items Widget
+          PopularItemWidget(),
+        ],
+      ),
+    );
+  }
+}
---
+++ food_delivery_app_ui/lib/widgets/popular_item_widget.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class PopularItemWidget extends StatelessWidget {
+  const PopularItemWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+        child: Row(
+          children: [
+            //Single item
+            //for(int i=0;i<10;i++)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 7),
+              child: Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withValues(alpha: 0.5),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 130,
+                        child: Image.asset('images/burger.png'),
+                      ),
+                      Text(
+                        'Гамбургер',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text('Вкусный бургер', style: TextStyle(fontSize: 15)),
+                      SizedBox(height: 12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '250 руб.',
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(Icons.favorite_border,color: Colors.red,size: 26,)
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 7),
+              child: Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withValues(alpha: 0.5),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 130,
+                        child: Image.asset('images/salan.png'),
+                      ),
+                      Text(
+                        'Гамбургер',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text('Вкусный бургер', style: TextStyle(fontSize: 15)),
+                      SizedBox(height: 12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '250 руб.',
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(Icons.favorite_border,color: Colors.red,size: 26,)
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
